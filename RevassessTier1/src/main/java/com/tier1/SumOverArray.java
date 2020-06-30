9d87725ca4b2d21941fffbd7d6e8ea864c0250cf
Ryan Gerold <ryan.gerold@gmail.com>
2020-06-30 14:13:51 -0400

    got 60 points

--- RevassessTier1/src/main/java/com/tier1/SumOverArray.java
+++ RevassessTier1/src/main/java/com/tier1/SumOverArray.java
@@ -3,16 +3,22 @@ package com.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		if (arr.length == 0)
+		if (arr == null)
 		{
 			return 0;
 		}
-		
-		Integer total = 0;
-		for (int i = 0; i < arr.length; i++)
+		if (arr.length > 0)
 		{
-			total += arr[i];
+			Integer total = 0;
+			for (int i = 0; i < arr.length; i++)
+			{
+				total += arr[i];
+			}
+			return total;
+		}else {
+			
+			return 0;
 		}
-		return total;
+		
 	}
 }
