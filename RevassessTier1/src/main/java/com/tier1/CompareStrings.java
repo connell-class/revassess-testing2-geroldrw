07cf1e9b7b9644b074cd64f9f5699c53a304a451
Ryan Gerold <ryan.gerold@gmail.com>
2020-06-30 11:11:55 -0400

    did the project

--- RevassessTier1/src/main/java/com/tier1/CompareStrings.java
+++ RevassessTier1/src/main/java/com/tier1/CompareStrings.java
@@ -3,6 +3,10 @@ package com.tier1;
 public class CompareStrings {
 
     public static boolean compareStrings(String s1, String s2){
+    	if (s1.contentEquals(s2))
+    	{
+    		return true;
+    	}
         return false;
     }
 }
--- RevassessTier1/src/main/java/com/tier1/SumOverArray.java
+++ RevassessTier1/src/main/java/com/tier1/SumOverArray.java
@@ -3,6 +3,16 @@ package com.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		if (arr.length == 0)
+		{
+			return 0;
+		}
+		
+		Integer total = 0;
+		for (int i = 0; i < arr.length; i++)
+		{
+			total += arr[i];
+		}
+		return total;
 	}
 }
